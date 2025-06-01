9416888c4333915530346eafefa0e63888db93a7
chakhva <aleksandre.chakhvashvili24@gmail.com>
2025-06-01 21:53:46 +0400

    add robot type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,3 +3,10 @@ export interface Cell {
   col: number;
   type: "empty" | "robot" | "task";
 }
+
+export interface Robot {
+  id: string;
+  position: [number, number];
+  target?: [number, number];
+  path: [number, number][];
+}
