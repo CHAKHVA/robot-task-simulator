bb7b46aab89aa8a3d624f6b55b5eb3d07846244d
niniigagnidze <ninigagnidze2005@gmail.com>
2025-06-01 23:28:52 +0400

    make grid more beautiful

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,11 @@
 import { Grid } from "@/components/grid";
 
 export default function Home() {
-  return <Grid />;
+  <div className="flex-1 p-4">
+    <div className="max-w-7xl mx-auto">
+      <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-4 relative">
+        <Grid />
+      </div>
+    </div>
+  </div>;
 }
